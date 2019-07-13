5ee399f47af21b753694abaa9e909e3d9500bfed
sxydh <sxydhgg@gmail.com>
2019-07-13 17:10:28 +0800

    build

--- mechat/web/config/index.js
+++ mechat/web/config/index.js
@@ -42,7 +42,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '/root/Desktop/keep/apache/tomcat/webapps/mechat'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
 
