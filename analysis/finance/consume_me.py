31e2ed5ac6b85124018c9746b8b315a400648616
sxydh <sxydhgg@gmail.com>
2019-09-23 20:29:34 +0800

    build

--- analysis/finance/consume_me.py
+++ analysis/finance/consume_me.py
@@ -5,32 +5,41 @@ import datetime
 import traceback
 
 
-def select(sql, param):
+def query(sql, data=""):
     conn = sqlite3.connect('../me.db')
     c = conn.cursor()
 
-    c.execute(sql, param)
+    c.execute(sql, data)
     rows = c.fetchall()
     conn.close()
-    print("select(sql=" + sql + ", param=" + str(param) + ") -> " + str(rows))
+    print("query(sql=" + sql + ", param=" + str(data) + ") -> " + str(rows))
     return rows
 
 
-def insert(list):
+def dml(dmls):
     conn = sqlite3.connect('../me.db')
     c = conn.cursor()
 
+    result = None
     try:
-        for ele in list:
-            print(ele)
-            result = c.execute(ele[0], ele[1])
-            print("insert(sql=" + ele[0] + ", param=" + str(ele[1]) + ") -> " + str(result))
+        for dml in dmls:
+            c.execute(dml[0], dml[1])
+            print("dml(sql=" + dml[0] + ", param=" + str(dml[1]) + ") -> " + str(c.fetchall()))
         conn.commit()
+        result = c
     except Exception:
-        print(Exception)
         conn.rollback()
         traceback.print_exc()
     conn.close()
+    return result
+
+
+def layout(obj):
+    obj.Layout()
+    upper = obj.GetParent()
+    while upper is not None:
+        upper.Layout()
+        upper = upper.GetParent()
 
 
 class ConsumeMe(wx.Panel):
@@ -39,7 +48,11 @@ class ConsumeMe(wx.Panel):
         wx.Panel.__init__(self, parent)
 
         self.box = wx.BoxSizer(wx.VERTICAL)
-        self.box.AddMany([Header(self), List(self), Live(self)])
+        header = Header(self)
+        list = List(self)
+        live = Live(self)
+        live.parent_list = list
+        self.box.AddMany([header, list, live])
         self.SetSizer(self.box)
 
 
@@ -68,15 +81,33 @@ class Header(wx.Panel):
 class List(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent)
-        self.update_data(event=None)
+        self.box = wx.BoxSizer(wx.VERTICAL)
+        self.SetSizer(self.box)
 
-    def update_data(self, event):
-        sql = " SELECT GROUP_CONCAT(c.id), c.date, MAX(a.name) account_name, MAX(ct.name) category_name, SUM(c.value), GROUP_CONCAT(c.name) names, GROUP_CONCAT(c.value) \"values\" FROM consume c "
+        self.update_data()
+
+    def update_data(self, event=None):
+        while len(self.box.GetChildren()) > 0:
+            self.box.Hide(self.box.GetChildren()[0].GetWindow())
+            self.box.Remove(0)
+        sql = " SELECT GROUP_CONCAT(c.id), c.date, a.name account, ct.name category, PRINTF(\"%.2f\", SUM(c.value) / 100.0) sum, GROUP_CONCAT(c.name) names, GROUP_CONCAT(PRINTF(\"%.2f\", c.value / 100.0)) \"values\" FROM consume c "
         sql += " INNER JOIN account a ON a.id = c.account_id "
         sql += " INNER JOIN category ct ON ct.id = c.category_id "
-        sql += " GROUP BY c.date "
-        rows = select(sql=sql, param=())
-        print(rows)
+        sql += " GROUP BY c.date, a.id, ct.id "
+        rs = query(sql=sql)
+        for r in rs:
+            ids = r[0].split(",")
+            date = r[1]
+            account = r[2]
+            category = r[3]
+            sum = r[4]
+            names = r[5].split(",")
+            values = r[6].split(",")
+            details = []
+            for i in range(0, len(ids)):
+                details.append((ids[i], names[i], values[i]))
+            self.box.Add(Row(self).load_data(date=date, account=account, category=category, sum=sum, details=details))
+        layout(self)
 
         if event is not None:
             event.Skip()
@@ -90,18 +121,15 @@ class Live(wx.Panel):
         self.box.Add(Row(self).load_new())
         self.SetSizer(self.box)
 
-    def reset(self, event):
+    def reset(self, event=None):
         children = self.box.GetChildren()
-        print(self.box.GetChildren())
         i = 0
         for child in children:
             self.box.Hide(child.GetWindow())
             self.box.Remove(i)
             i += 1
-        print(self.box.GetChildren())
         self.box.Add(Row(self).load_new())
-        self.box.Layout()
-        print(self.box.GetChildren())
+        layout(self)
 
         if event is not None:
             event.Skip()
@@ -156,7 +184,7 @@ class Row(wx.Panel):
         self.category_id = -1
         self.category.SetFont(font)
 
-        rows = select(" SELECT id FROM user WHERE name = ? ", ("me",))
+        rows = query(" SELECT id FROM user WHERE name = ? ", ("me",))
         if len(rows) > 0:
             self.user_id = rows[0][0]
         else:
@@ -171,7 +199,7 @@ class Row(wx.Panel):
 
         self.left_box.AddMany([self.date, self.week, self.account, self.category, self.sum])
 
-    def category_shortcut(self, event):
+    def category_shortcut(self, event=None):
         key_code = event.GetKeyCode()
         print("key_code: " + str(key_code))
         text_ctrl = event.GetEventObject()
@@ -186,7 +214,7 @@ class Row(wx.Panel):
         if event is not None:
             event.Skip()
 
-    def account_shortcut(self, event):
+    def account_shortcut(self, event=None):
         key_code = event.GetKeyCode()
         print("key_code: " + str(key_code))
         text_ctrl = event.GetEventObject()
@@ -201,7 +229,7 @@ class Row(wx.Panel):
         if event is not None:
             event.Skip()
 
-    def date_shortcut(self, event):
+    def date_shortcut(self, event=None):
         key_code = event.GetKeyCode()
         print("key_code: " + str(key_code))
         text_ctrl = event.GetEventObject()
@@ -212,13 +240,30 @@ class Row(wx.Panel):
 
         if self.key_codes[len(self.key_codes) - 2] == 308 and self.key_codes[len(self.key_codes) - 1] == 59:
             text_ctrl.SetValue(datetime.datetime.today().strftime('%Y-%m-%d'))
+            text_ctrl.SetInsertionPointEnd()
         elif self.key_codes[len(self.key_codes) - 2] == 308 and self.key_codes[len(self.key_codes) - 1] == 65:
             text_ctrl.SetSelection(0, len(text_ctrl.GetValue()))
-
+        elif self.key_codes[len(self.key_codes) - 2] == 308 and self.key_codes[len(self.key_codes) - 1] == 370:
+            print()
+        elif self.key_codes[len(self.key_codes) - 1] == wx.WXK_DELETE:
+            msg = wx.MessageBox("sure to delete?", "Info", wx.OK | wx.CANCEL | wx.ICON_INFORMATION)
+            if msg == 4:
+                dmls = []
+                for detail_box in self.detail_boxes:
+                    dmls.append((" DELETE FROM consume WHERE id = ? ", (detail_box.id,)))
+                result = dml(dmls)
+                if result is not None:
+                    self.GetParent().box.Hide(self)
+                    children = self.GetParent().box.GetChildren()
+                    for i in range(0, len(children)):
+                        if children[i].GetWindow().GetId() == self.GetId():
+                            self.GetParent().box.Remove(i)
+                            break
+                    layout(self)
         if event is not None:
             event.Skip()
 
-    def week_cal(self, event):
+    def week_cal(self, event=None):
         if self.date.GetValue() is not None:
             split = self.date.GetValue().split("-")
             if len(split) == 3:
@@ -244,19 +289,19 @@ class Row(wx.Panel):
         if event is not None:
             event.Skip()
 
-    def check_date(self, event):
+    def check_date(self, event=None):
         if re.fullmatch(pattern="^\d{4}(\-)\d{2}(\-)\d{2}$", string=self.date.GetValue()) is None:
             self.date.SetForegroundColour(wx.RED)
         else:
             self.date.SetForegroundColour(wx.BLACK)
 
-        self.week_cal(event=None)
+        self.week_cal()
 
         if event is not None:
             event.Skip()
 
-    def check_account(self, event):
-        rows = select(" SELECT id FROM account WHERE name = ? ", (event.GetEventObject().GetValue(),))
+    def check_account(self, event=None):
+        rows = query(" SELECT id FROM account WHERE name = ? ", (event.GetEventObject().GetValue(),))
         if len(rows) == 0:
             self.account.SetForegroundColour(wx.RED)
         else:
@@ -266,8 +311,8 @@ class Row(wx.Panel):
         if event is not None:
             event.Skip()
 
-    def check_category(self, event):
-        rows = select(" SELECT id FROM category WHERE name = ? ", (event.GetEventObject().GetValue(),))
+    def check_category(self, event=None):
+        rows = query(" SELECT id FROM category WHERE name = ? ", (event.GetEventObject().GetValue(),))
         if len(rows) == 0:
             self.category.SetForegroundColour(wx.RED)
         else:
@@ -281,16 +326,26 @@ class Row(wx.Panel):
         self.add_detail()
         return self
 
-    def add_detail(self):
+    def load_data(self, date, account, category, sum, details):
+        self.date.SetValue(date)
+        self.week_cal()
+        self.account.SetValue(account)
+        self.category.SetValue(category)
+        self.sum.SetValue(str(sum))
+        for detail in details:
+            self.add_detail(id=detail[0], name=detail[1], value=detail[2])
+        return self
+
+    def add_detail(self, id=None, name="", value=""):
         detail_box = wx.BoxSizer(wx.VERTICAL)
+        detail_box.id = id
         self.right_box.Add(detail_box)
 
-        detail_name = wx.TextCtrl(self, id=wx.ID_ANY, value="", size=(self.width_cell / 2, self.height_cell / 2),
+        detail_name = wx.TextCtrl(self, id=wx.ID_ANY, value=name, size=(self.width_cell / 2, self.height_cell / 2),
                                   style=wx.TE_PROCESS_ENTER)
-        detail_value = wx.TextCtrl(self, id=wx.ID_ANY, value="", size=(self.width_cell / 2, self.height_cell / 2),
+        detail_value = wx.TextCtrl(self, id=wx.ID_ANY, value=value, size=(self.width_cell / 2, self.height_cell / 2),
                                    style=wx.TE_PROCESS_ENTER)
-        detail_box.Add(detail_name)
-        detail_box.Add(detail_value)
+        detail_box.AddMany([detail_name, detail_value])
 
         detail_name.Bind(wx.EVT_KEY_DOWN, self.detail_shortcut)
         detail_value.Bind(wx.EVT_KEY_DOWN, self.detail_shortcut)
@@ -298,10 +353,9 @@ class Row(wx.Panel):
 
         self.detail_boxes.append(detail_box)
 
-        self.Layout()
-        self.GetParent().Layout()
+        layout(self)
 
-    def process_detail_val(self, event):
+    def process_detail_val(self, event=None):
         sum_val = 0
         for child in self.detail_boxes:
             detail_value = child.GetChildren()[1].GetWindow()
@@ -318,7 +372,7 @@ class Row(wx.Panel):
         if event is not None:
             event.Skip()
 
-    def detail_shortcut(self, event):
+    def detail_shortcut(self, event=None):
         key_code = event.GetKeyCode()
         print("key_code: " + str(key_code))
         self.key_codes.append(key_code)
@@ -340,14 +394,19 @@ class Row(wx.Panel):
             detail_box = self.get_detail_sizer(event.GetEventObject())
             if len(self.detail_boxes) == 1:
                 return
+            if detail_box.id is not None:
+                dmls = [(" DELETE FROM consume WHERE id = ? ", (detail_box.id,))]
+                result = dml(dmls)
+                if result is None:
+                    return
+            self.detail_boxes.remove(detail_box)
             self.right_box.Hide(detail_box)
             self.right_box.Remove(detail_box)
-            self.detail_boxes.remove(detail_box)
-            self.process_detail_val(event=None)
-            self.right_box.Layout()
+            self.process_detail_val()
+            layout(self)
 
         elif self.key_codes[len(self.key_codes) - 3] == 308 and self.key_codes[len(self.key_codes) - 2] == 306 and self.key_codes[len(self.key_codes) - 1] == 83:
-            self.save()
+            self.save_or_update()
 
         if event is not None:
             event.Skip()
@@ -358,25 +417,45 @@ class Row(wx.Panel):
                 if widget.GetId() == child.GetWindow().GetId():
                     return detail_box
 
-    def save(self):
-        inserts = []
+    def save_or_update(self):
+        dmls = []
 
         for detail_box in self.detail_boxes:
             detail = detail_box.GetChildren()
+            detail_id = detail_box.id
             detail_name = detail[0].GetWindow().GetValue()
-            detail_value = detail[1].GetWindow().GetValue()
-            sql = " INSERT INTO consume (date, name, value, ctime, utime, account_id, category_id, user_id) "
-            sql += " VALUES (?, ?, ?, DATETIME(CURRENT_TIMESTAMP,'localtime'), DATETIME(CURRENT_TIMESTAMP,'localtime'), ?, ?, ?) "
-            param = (self.date.GetValue(), detail_name, detail_value, self.account_id, self.category_id, self.user_id)
+            detail_value = float(detail[1].GetWindow().GetValue()) * 100
+            if detail_id is None:
+                param = (self.date.GetValue(), detail_name, detail_value, self.account_id, self.category_id, self.user_id)
+                sql = " INSERT INTO consume (date, name, value, ctime, utime, account_id, category_id, user_id) "
+                sql += " VALUES (?, ?, ?, DATETIME(CURRENT_TIMESTAMP,'localtime'), DATETIME(CURRENT_TIMESTAMP,'localtime'), ?, ?, ?) "
+            else:
+                param = (self.date.GetValue(), detail_name, detail_value, self.account_id, self.category_id, self.user_id, detail_id)
+                sql = " UPDATE consume SET id = id "
+                sql += ", date = ? "
+                sql += ", name = ? "
+                sql += ", value = ? "
+                sql += ", utime = DATETIME(CURRENT_TIMESTAMP,'localtime') "
+                sql += ", account_id = ? "
+                sql += ", category_id = ? "
+                sql += ", user_id = ? "
+                sql += " WHERE id = ? "
             for val in param:
                 if val is None or str(val) == "":
                     wx.MessageBox("there is null value in param", "Info",
                                   wx.OK | wx.ICON_INFORMATION)
                     return
-            inserts.append((sql, param))
+            dmls.append((sql, param))
 
-        insert(inserts)
-        self.GetParent().reset(event=None)
+        if len(dmls) > 0:
+            result = dml(dmls)
+            if result is not None:
+                self.detail_boxes.clear()
+                if detail_id is None:
+                    self.GetParent().reset()
+                    self.GetParent().parent_list.update_data()
+                else:
+                    self.GetParent().update_data()
 
 
 def main():
