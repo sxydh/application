189f8c19d51f5db9f73c5cfb0d0f9da4629d1e85
sxydh <sxydhgg@gmail.com>
2019-10-21 20:20:00 +0800

    build

--- charging-system/client/web/manage/config/dev.env.js
+++ charging-system/client/web/manage/config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ROOT: '"http://localhost:8080/"'
+  API_ROOT: '"http://www.bhe.net.cn:6666/char-server-manage"'
 })
